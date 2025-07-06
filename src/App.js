28a4c60b153b843cdff69c9c0721eb0713be5bd3
Rhythm Sarma <rhythmsarma@Rhythms-MacBook-Air.local>
2025-07-06 21:45:39 +0530

    Fix signup.jsx to store userId after login/signup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Storefront from './storefront';
 import Customize from './customize';
 import Products from './products';
@@ -27,19 +27,19 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Signup />} />
-        <Route path="/storefront" element={userId ? <Storefront /> : <Signup />} />
-        <Route path="/products" element={userId ? <Products /> : <Signup />} />
-        <Route path="/orders" element={userId ? <Orders /> : <Signup />} />
+        <Route path="/storefront" element={userId ? <Storefront /> : <Navigate to="/signup" />} />
+        <Route path="/products" element={userId ? <Products /> : <Navigate to="/signup" />} />
+        <Route path="/orders" element={userId ? <Orders /> : <Navigate to="/signup" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/product/:id" element={<InProduct />} />
-        <Route path="/payment" element={userId ? <Payment /> : <Signup />} />
-        <Route path="/customize" element={userId ? <Customize /> : <Signup />} />
-        <Route path="/preview" element={userId ? <ViewSite /> : <Signup />} />
-        <Route path="/orderform" element={userId ? <OrderForm /> : <Signup />} />
+        <Route path="/payment" element={userId ? <Payment /> : <Navigate to="/signup" />} />
+        <Route path="/customize" element={userId ? <Customize /> : <Navigate to="/signup" />} />
+        <Route path="/preview" element={userId ? <ViewSite /> : <Navigate to="/signup" />} />
+        <Route path="/orderform" element={userId ? <OrderForm /> : <Navigate to="/signup" />} />
         <Route path="/shop/:storeId" element={<ViewSite />} />
         <Route path="/shop/:storeId/product/:id" element={<InProduct />} />
         <Route path="/shop/:storeId/orderform" element={<OrderForm />} />
-        <Route path="/navview" element={userId ? <NavView /> : <Signup />} />
+        <Route path="/navview" element={userId ? <NavView /> : <Navigate to="/signup" />} />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </Router>
