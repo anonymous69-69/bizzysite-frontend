b5b87f1dd2f043c3cf1cb9d67d67515e0a566595
Rhythm Sarma <rhythmsarma@Rhythms-MacBook-Air.local>
2025-07-08 13:39:52 +0530

    Fix signup.jsx to store userId after login/signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // <- this is important
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 0.5s ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
