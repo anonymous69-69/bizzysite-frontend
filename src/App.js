bf5cc6f8d7da624cb0ee3b78204b487ecbac8088
Rhythm Sarma <rhythmsarma@Rhythms-MacBook-Air.local>
2025-07-13 04:14:00 +0530

    Fix signup.jsx to store userId after login/signup

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/store/:storeName" element={<ViewSite />} />
         <Route path="/store/:slug" element={<ViewSite />} />
-        <Route path="/store/:slug/product/:productId" element={<InProduct />} />
+        <Route path="/:slug/product/:productId" element={<InProduct />} />
         <Route path="/:slug" element={<ViewSite />} />
 
         <Route path="*" element={<div>404 Not Found</div>} />
