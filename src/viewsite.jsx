2e7ad8f77a141ff35aae4ae9daee47c34148f18a
Rhythm Sarma <rhythmsarma@Rhythms-MacBook-Air.local>
2025-09-22 14:22:10 +0530

    Finalize currency list and search updates

--- src/viewsite.jsx
+++ src/viewsite.jsx
@@ -859,7 +859,7 @@ const ViewSite = () => {
               </h4>
               <ul className="space-y-2 text-gray-300 text-sm sm:text-base">
                 <li>Email: your-store@bizzysite.shop</li>
-                <li>Phone: +91 7086758292</li>
+                
               </ul>
             </div>
           </div>
